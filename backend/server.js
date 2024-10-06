9f98f544c99b1cee95f7bb6cd4ff743f0a4494d2
Ritesh Singh <riteshsingh@Riteshs-Laptop.local>
2024-10-06 15:31:15 +0530

    Add missing auth module

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
-const authRoutes = require('backend/routes/Auth.js');
+const authRoutes = require('./routes/Auth.js');
 
 const app = express();
 
