40795a174ee2465549091e5cb415dbeb3b66ba0e
Ritesh Singh <riteshsingh@Riteshs-Laptop.local>
2024-10-06 23:05:17 +0530

    changes into frontend index.js

--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -4,17 +4,19 @@ import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 import  AuthProvider  from './components/AuthContext';
-// import { GoogleOAuthProvider } from '@react-oauth/google';
-// const clientId = process.env.REACT_APP_GOOGLE_CLIENT_ID;
+import { GoogleOAuthProvider } from '@react-oauth/google';
+const clientId = '1008128593733-8imhmu89203kj8krchi7t73iudbkd5gp.apps.googleusercontent.com';
 
 
 // import { AuthProvider } from './components/AuthContext'; // Import AuthProvider
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthProvider>
-      <App />
-    </AuthProvider>
+    <GoogleOAuthProvider clientId={clientId}>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </GoogleOAuthProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
