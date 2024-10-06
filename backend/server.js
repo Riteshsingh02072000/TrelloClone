401a7a76eef14c7f37da6ac30d014488d39d3cf6
Ritesh Singh <riteshsingh@Riteshs-Laptop.local>
2024-10-06 18:11:45 +0530

    m

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const passport = require('passport'); // Import Passport
+const session = require('express-session');
 
 const authRoutes = require('./routes/Auth.js');
 
@@ -11,6 +13,19 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || 'your-session-secret',
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use('/api/auth', authRoutes);
 
 // Routes
 app.use('/api/auth', authRoutes);
